f786aecd3df056b831ae16875957565d032416a5
chirag <sharmachirag242004@gmail.com>
2025-10-06 12:52:24 +0530

    signup logic done

--- v2/frontend/src/App.jsx
+++ v2/frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Signup from "./Pages/Signup";
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<div></div>} />
+      </Routes>
     </div>
   )
 }
---
+++ v2/frontend/src/Pages/Signup.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useState } from "react";
+
+function Signup() {
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-300">
+      <div className="bg-white shadow-lg rounded-2xl p-8 w-full max-w-md">
+
+        <h2 className="text-2xl font-bold text-center mb-6 text-gray-800">
+          Create an Account
+        </h2>
+
+        <div className="flex flex-col space-y-4">
+          <input type="text" placeholder="Full Name"
+              onChange={(e) => {
+                setFullName(e.target.value);
+              }}
+            className="border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+
+          <input type="email" placeholder="Email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            className="border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+
+          <input type="password" placeholder="Password"
+             onChange={(e) => {
+                setPassword(e.target.value);
+             }}
+            className="border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+
+          <button className="bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition duration-200 font-semibold"
+              onClick={() => {
+                fetch('http://localhost:3000/api/v2/signup', {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        fullName: fullName,
+                        email: email,
+                        password: password
+                    })
+                 })
+              }}
+          >
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
